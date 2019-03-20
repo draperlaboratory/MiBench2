5a6743d24b92a504c9f29cbc4c10f74553f2edd3
Tynan McAuley <tynan@galois.com>
2019-03-20 11:23:46 -0400

    Customized common Makefile.
    Compile flags mostly taken from here:
        https://gitlab-ext.galois.com/ssith/coremark/blob/add2f1026e1fe005febca8d6f500547e192a3960/riscv-bare-metal/core_portme.mak

--- Makefile.mk
+++ Makefile.mk
@@ -1,37 +1,83 @@
-TC_PATH = /usr
-TOOLCHAIN = arm-none-eabi
-ARMGNU = $(TC_PATH)/bin/$(TOOLCHAIN)
-CC = $ arm-none-eabi-gcc
-CHIP = cortex-m0plus
-OPTLVL = -O3 
+CC      := riscv64-unknown-elf-gcc
+OBJDUMP := riscv64-unknown-elf-objdump
+OBJCOPY := riscv64-unknown-elf-objcopy
 
-#FLAGS = -DBARE_METAL -Wall $(OPTLVL) -target $(TOOLCHAIN) -mcpu=$(CHIP) -mthumb --specs=nosys.specs -nostartfiles -ffreestanding -std=c99 -fomit-frame-pointer -fno-optimize-sibling-calls #-static
-FLAGS = -DBARE_METAL -Wall $(OPTLVL) -march=armv6-m -mcpu=$(CHIP) -mthumb -msoft-float --specs=nosys.specs -nostartfiles -ffreestanding -std=c99 -fomit-frame-pointer -fno-optimize-sibling-calls 
-#LIBS = --start-group -lm -lc -lbuiltins --end-group
-LIBS = -lnosys -lc -lm
-OBJS := vectors.o putget.o supportFuncs.o LED.o $(OBJS)
-#LINKDIR=-L$(TC_PATH)/$(TOOLCHAIN)/lib
-LINKDIR=
-INCLIB = -I$(TC_PATH)/$(TOOLCHAIN)/include 
+# Allow users to override the UART's baud rate.
+UART_BAUD_RATE ?= 115200
 
-all: main.elf 
+# Make sure user explicitly defines the target GFE platform.
+ifeq ($(GFE_TARGET),P1)
+	RISCV_FLAGS := -march=rv32imac -mabi=ilp32
+	# 83.33 MHz clock
+	CLOCKS_PER_SEC := 83330000
+else ifeq ($(GFE_TARGET),P2)
+	RISCV_FLAGS := -march=rv64imafdc -mabi=lp64d
+	# 50 MHz clock
+	CLOCKS_PER_SEC := 50000000
+else ifeq ($(GFE_TARGET),P3)
+$(error P3 target has not been tested yet, use P1 or P2)
+else
+$(error Please define GFE_TARGET to P1, P2, or P3 (e.g. make GFE_TARGET=P1))
+endif
+
+# Define sources and compilation outputs.
+COMMON_DIR := ..
+LINKER_SCRIPT := $(COMMON_DIR)/test.ld
+COMMON_ASM_SRCS := \
+	$(COMMON_DIR)/crt.S
+COMMON_C_SRCS := \
+	$(COMMON_DIR)/syscalls.c \
+	$(COMMON_DIR)/uart_16550.c \
+	$(COMMON_DIR)/cvt.c \
+	$(COMMON_DIR)/ee_printf.c
+COMMON_OBJS := \
+	$(patsubst %.c,%.o,$(notdir $(COMMON_C_SRCS))) \
+	$(patsubst %.S,%.o,$(notdir $(COMMON_ASM_SRCS)))
+OBJS := $(COMMON_OBJS) $(OBJS)
+
+# Define compile and load/link flags.
+CFLAGS := \
+	$(RISCV_FLAGS) \
+	-DBARE_METAL \
+	-DCLOCKS_PER_SEC=$(CLOCKS_PER_SEC) \
+	-DUART_BAUD_RATE=$(UART_BAUD_RATE) \
+	-O2 \
+	-Wall \
+	-mcmodel=medany \
+	-static \
+	-std=gnu99 \
+	-ffast-math \
+	-fno-common \
+	-fno-builtin-printf \
+	-I$(COMMON_DIR)
+ASFLAGS := $(CFLAGS)
+LDFLAGS := \
+	-static \
+	-nostdlib \
+	-nostartfiles \
+	-lm \
+	-lc \
+	-lgcc \
+	-T $(LINKER_SCRIPT)
+
+all: main.elf
 
 %.o: %.s
-	$(CC) $(FLAGS) $(INCLIB) -c -o $@ $<
+	$(CC) $(ASFLAGS) -c -o $@ $<
 
-%.o: ../%.s
-	$(CC) $(FLAGS) $(INCLIB) -c -o $@ $<
+%.o: ../%.S
+	$(CC) $(ASFLAGS) -c -o $@ $<
 
 %.o: %.c
-	$(CC) ${FLAGS} $(INCLIB) -c -o $@ $< 
+	$(CC) $(CFLAGS) -c -o $@ $<
 
 %.o: ../%.c
-	$(CC) ${FLAGS} $(INCLIB) -c -o $@ $< 
+	$(CC) $(CFLAGS) -c -o $@ $<
 
-main.elf: $(OBJS) ../vectors.s ../putget.s ../supportFuncs.c ../LED.s
-	$(CC) $(FLAGS) -u _printf_float -T ../memmap $(LINKDIR) $(OBJS) -o main.elf $(LIBS)
-	$(TOOLCHAIN)-objdump -d main.elf > main.lst
-	$(TOOLCHAIN)-objcopy main.elf main.bin -O binary
+main.elf: $(OBJS) $(COMMON_C_SRCS) $(COMMON_ASM_SRCS)
+	$(CC) $(CFLAGS) $(OBJS) -o main.elf $(LDFLAGS)
+	$(OBJDUMP) --disassemble-all main.elf > main.lst
+	$(OBJCOPY) main.elf main.bin -O binary
 
 clean: more_clean
 	rm -rf *.o *.elf output* *.lst *.bin *~
