c73a3da97a0018b312d36b7f7b9c8afe7d0566bb
Tynan McAuley <tynan@galois.com>
2019-03-25 17:34:18 -0400

    Fixed compilation errors in `limits` benchmark.
    There were two issues:
    
        1. There were UART methods that referenced ARM assembly symbols. Got
           rid of all the UART-related code, we don't need the benchmark to
           print anything.
        2. The benchmark didn't include `bareBench.h`, which is needed to
           time and report the benchmark results.

--- limits/limit_test.c
+++ limits/limit_test.c
@@ -1,31 +1,4 @@
-
-void PUT32 ( unsigned int, unsigned int);
-
-#define THUL_UART_BASE 0xE0000000
-//------------------------------------------------------------------------
-void uart_putc ( unsigned int c )
-{
-    PUT32(THUL_UART_BASE+0x0,c);
-}
-//------------------------------------------------------------------------
-void hexstring ( unsigned int d )
-{
-    //unsigned int ra;
-    unsigned int rb;
-    unsigned int rc;
-
-    rb=32;
-    while(1)
-    {
-        rb-=4;
-        rc=(d>>rb)&0xF;
-        if(rc>9) rc+=0x37; else rc+=0x30;
-        uart_putc(rc);
-        if(rb==0) break;
-    }
-    uart_putc(0x0D);
-    uart_putc(0x0A);
-}
+#include "../bareBench.h"
 
 extern unsigned short ult ( unsigned short a, unsigned short b);
 extern unsigned short slt ( short a, short b);
@@ -46,68 +19,50 @@ unsigned short limit_test ( void )
     short sa;
 
     for(ra=0;;ra++) if(ult(ra,7)==0) break;
-    hexstring(ra);
     if(ra!=7) return(1);
 
     for(ra=0xF000;;ra++) if(ult(ra,0xF007)==0) break;
-    hexstring(ra);
     if(ra!=0xF007) return(1);
 
     for(sa=-7;;sa++) if(slt(sa,7)==0) break;
-    hexstring(sa);
     if(sa!=7) return(1);
 
     for(sa=-17;;sa++) if(slt(sa,-7)==0) break;
-    hexstring(sa);
     if(sa!=-7) return(1);
 
     for(ra=0;;ra++) if(ulte(ra,7)==0) break;
-    hexstring(ra);
     if(ra!=8) return(1);
 
     for(ra=0xF000;;ra++) if(ulte(ra,0xF007)==0) break;
-    hexstring(ra);
     if(ra!=0xF008) return(1);
 
     for(ra=0xF000;;ra++) if(ulte(ra,7)==0) break;
-    hexstring(ra);
     if(ra!=0xF000) return(1);
 
     for(sa=-7;;sa++) if(slte(sa,7)==0) break;
-    hexstring(sa);
     if(sa!=8) return(1);
 
     for(sa=-17;;sa++) if(slte(sa,-7)==0) break;
-    hexstring(sa);
     if(sa!=-6) return(1);
 
     for(ra=0;;ra++) if(ugt(ra,7)) break;
-    hexstring(ra);
     if(ra!=8) return(1);
 
     for(ra=0xF000;;ra++) if(ugt(ra,0xF007)) break;
-    hexstring(ra);
     if(ra!=0xF008) return(1);
 
     for(ra=0;;ra++) if(ugte(ra,7)) break;
-    hexstring(ra);
     if(ra!=7) return(1);
 
     for(ra=0xF000;;ra++) if(ugte(ra,0xF007)) break;
-    hexstring(ra);
     if(ra!=0xF007) return(1);
 
     return(0);
 
 }
-unsigned short main ( void )
+int main ( void )
 {
-    hexstring(0x1234);
-
-    if(limit_test()) hexstring(0xBAD);
-
-    hexstring(0x1234);
-    return(0);
+    return(limit_test());
 }
 
 
