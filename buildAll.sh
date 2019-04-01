2d00d66fa35b3d69b24044ed230d55da54b5f8e7
Tynan McAuley <tynan@galois.com>
2019-04-01 09:39:06 -0400

    Copy ELF as compilation output, not bare binary.
    The `main.bin` compilation output is the binary-only result of `objcopy
    -O binary`, which is not a standalone executable. `main.elf` is the
    executable we want to run on the FPGA.

--- buildAll.sh
+++ buildAll.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find . -d 1 -type d \( ! -name .\* \) -exec bash -c "cd {} && make GFE_TARGET=$1 clean && make GFE_TARGET=$1 && cp main.bin ../{}.bin && make GFE_TARGET=$1 clean" \;
+find . -d 1 -type d \( ! -name .\* \) -exec bash -c "cd {} && make GFE_TARGET=$1 clean && make GFE_TARGET=$1 && cp main.elf ../{}.bin && make GFE_TARGET=$1 clean" \;
